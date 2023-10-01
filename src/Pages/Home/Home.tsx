bfb56922fae41764ab1f6c5f70a7adbbade05165
khalidAdell <kadel2034@gmail.com>
2023-10-01 03:34:37 +0200

    d

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -31,31 +31,33 @@ const Home = () => {
   return (
     <section className="homePage container">
       {/* landing */}
-      <div className="landing">
-        <div className="landing-text">
-          <div className="landing-latesets">latesets news</div>
-          <Link to={landingNew.url} target="_blank">
-            <h2 className="landing-text-name">
-              {landingNew.name.slice(0, 65)}...
-            </h2>
-          </Link>
-          <p className="landing-text-date">
-            {landingNew.datePublished.slice(0, 10)}
-          </p>
-          <p className="landing-text-description">
-            {landingNew.description.length > 100
-              ? `${landingNew.description.slice(0, 130)}...`
-              : landingNew.description}
-          </p>
+      {landingNew && (
+        <div className="landing">
+          <div className="landing-text">
+            <div className="landing-latesets">latesets news</div>
+            <Link to={landingNew.url} target="_blank">
+              <h2 className="landing-text-name">
+                {landingNew.name.slice(0, 65)}...
+              </h2>
+            </Link>
+            <p className="landing-text-date">
+              {landingNew.datePublished.slice(0, 10)}
+            </p>
+            <p className="landing-text-description">
+              {landingNew.description.length > 100
+                ? `${landingNew.description.slice(0, 130)}...`
+                : landingNew.description}
+            </p>
+          </div>
+          <img
+            className="landing-img"
+            src={
+              "	https://blockchainmedia.id/wp-content/uploads/2023/07/halving-bitcoin.webp"
+            }
+            alt={landingNew.name}
+          />
         </div>
-        <img
-          className="landing-img"
-          src={
-            "	https://blockchainmedia.id/wp-content/uploads/2023/07/halving-bitcoin.webp"
-          }
-          alt={dataNews.name}
-        />
-      </div>
+      )}
       {/* coins stats */}
       <div className="stats">
         <div>
--- src/Pages/News/News.tsx
+++ src/Pages/News/News.tsx
@@ -14,31 +14,33 @@ const News = () => {
   return (
     <section>
       {/* landing */}
-      <div className="landing">
-        <div className="landing-text">
-          <div className="landing-latesets">latesets news</div>
-          <Link to={landingNew.url} target="_blank">
-            <h2 className="landing-text-name">
-              {landingNew.name.slice(0, 65)}...
-            </h2>
-          </Link>
-          <p className="landing-text-date">
-            {landingNew.datePublished.slice(0, 10)}
-          </p>
-          <p className="landing-text-description">
-            {landingNew.description.length > 100
-              ? `${landingNew.description.slice(0, 130)}...`
-              : landingNew.description}
-          </p>
+      {landingNew && (
+        <div className="landing">
+          <div className="landing-text">
+            <div className="landing-latesets">latesets news</div>
+            <Link to={landingNew.url} target="_blank">
+              <h2 className="landing-text-name">
+                {landingNew.name.slice(0, 65)}...
+              </h2>
+            </Link>
+            <p className="landing-text-date">
+              {landingNew.datePublished.slice(0, 10)}
+            </p>
+            <p className="landing-text-description">
+              {landingNew.description.length > 100
+                ? `${landingNew.description.slice(0, 130)}...`
+                : landingNew.description}
+            </p>
+          </div>
+          <img
+            className="landing-img"
+            src={
+              "https://bitcoin.pl/wp-content/uploads/2014/05/Jak-dziala-bitcoin.jpg"
+            }
+            alt={landingNew.name}
+          />
         </div>
-        <img
-          className="landing-img"
-          src={
-            "https://bitcoin.pl/wp-content/uploads/2014/05/Jak-dziala-bitcoin.jpg"
-          }
-          alt={dataNews.name}
-        />
-      </div>
+      )}
       <div className="subTitle">
         <h2>Latest coins news</h2>
       </div>
